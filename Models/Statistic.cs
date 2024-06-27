9f3b715a8dd2ad2c95d8bbfe8b43dc4cdc4f8d20
Nariman Biiazbekov <narbiiaz@gmail.com>
2024-06-27 13:50:50 +0600

    Добавлены модели базы данны. Создан контекст приложения

---
+++ Models/Statistic.cs
@@ -0,0 +1,12 @@
+﻿namespace YouCan.Models;
+
+public class Statistic
+{
+    public int Id { get; set; }
+    public int Streak { get; set; }
+    public int TotalExperience { get; set; }
+    public int StudyMinutes { get; set; }
+
+    public int UserId { get; set; }
+    public User? User { get; set; }
+}
---
+++ Models/Test.cs
@@ -0,0 +1,17 @@
+﻿namespace YouCan.Models;
+
+public class Test
+{
+    public int Id { get; set; }
+    public int GainingExperience { get; set; }
+    public int TimeForTest { get; set; }
+
+    public int? LessonId { get; set; }
+    public Lesson? Lesson { get; set; }
+
+    public List<Question> Questions { get; set; }
+    public Test()
+    {
+        Questions = new List<Question>();
+    }
+}
