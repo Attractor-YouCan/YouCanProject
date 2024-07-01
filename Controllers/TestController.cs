215a800b4d774a19c2e9839e080da071f4336fc7
Titov Aleksandr <brgalex910@gmail.com>
2024-07-01 14:27:01 +0600

    Создал контроллер Test с с методами которые возвращают представления TakeTest, CreateTest, Main

---
+++ Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using YouCan.Models;
+
+namespace YouCan.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Main()
+        {
+            return View();
+        }
+        public IActionResult CreateTest()
+        {
+            return View();
+        }
+        public IActionResult TakeTest()
+        {
+            return View();
+        }
+    }
+}
