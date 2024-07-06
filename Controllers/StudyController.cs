1d14b73ec0f2bf42955d0106e51f874bc555300b
Imanbekov Tologon <imanbekovtologon4@gmail.com>
2024-07-06 15:13:25 +0600

    Worked on test passing logic

--- Controllers/StudyController.cs
+++ Controllers/StudyController.cs
@@ -26,24 +26,44 @@ public class StudyController : Controller
 
     public async Task<IActionResult> Lessons(int subTopicId)
     {
-        List<Lesson> lessons = _db.Lessons.Where(l => l.SubtopicId == subTopicId).ToList();
+        List<Lesson>? lessons = _db.Lessons.Where(l => l.SubtopicId == subTopicId).ToList();
+        if (lessons == null)
+            return NotFound("NO lessons in this topic !");
         User? currentUser = await _userManager.GetUserAsync(User);
-        UserLessons? userLessons = null;
-        if (currentUser!= null)
-            userLessons = await _db.UserLessons.FirstOrDefaultAsync(ul => ul.UserId == currentUser.Id);
+        if (currentUser == null)
+            return NotFound("No user");
+        
+        UserLessons? userLessons = await _db.UserLessons
+            .FirstOrDefaultAsync(ul => ul.UserId == currentUser.Id
+                                       && ul.SubtopicId == subTopicId);
         if (userLessons == null)
-            ViewBag.CurrentLessonLevel = 0;
-        else
-            ViewBag.CurrentLessonLevel = userLessons.PassedLevel;
+        {
+            userLessons = new UserLessons() { UserId = currentUser.Id, SubtopicId = subTopicId, PassedLevel = 0, IsPassed = true};
+            _db.UserLessons.Add(userLessons);
+            await _db.SaveChangesAsync();
+        }
+        ViewBag.CurrentLessonLevel = userLessons.PassedLevel;
         return View(lessons);
     }
 
     
     public async Task<IActionResult> Lesson(int lessonId)
     {
-        //User? currentUser = await _userManager.GetUserAsync(User);
-        var lesson = await _db.Lessons.FirstOrDefaultAsync(l => l.Id == lessonId);
-        return View(lesson);
+        User? currentUser = await _userManager.GetUserAsync(User);
+        if (currentUser == null)
+            return NotFound("No User!");
+        Lesson? lesson = await _db.Lessons.FirstOrDefaultAsync(l => l.Id == lessonId);
+        if (lesson == null)
+            return NotFound("No Lesson!");
+        UserLessons? userLessons = await _db.UserLessons
+            .FirstOrDefaultAsync(ul => ul.UserId == currentUser.Id
+            && ul.SubtopicId == lesson.SubtopicId);
+        if (userLessons == null)
+            return NotFound("NO UserLesson!");
+        if (userLessons.PassedLevel >= lesson.LessonLevel || userLessons.PassedLevel+1 == lesson.LessonLevel)
+            return View(lesson);
+        
+        return NotFound("Пройдите предыдущий урок, чтобы открыть");
     }
 
     public async Task<IActionResult> Test(int lessonId)
@@ -51,6 +71,51 @@ public class StudyController : Controller
         Test? test = await _db.Tests.Include(q => q.Questions)
             .ThenInclude(q => q.Answers)
             .FirstOrDefaultAsync(t => t.LessonId == lessonId);
+        if (test == null)
+            return NotFound("No Test!");
         return View(test);
     }
+
+    [HttpPost]
+    public async Task<IActionResult> Test([FromBody] List<TestAnswersModel> selectedAnswers)
+    {
+        User? currentUser = await _userManager.GetUserAsync(User);
+        Test? test = await _db.Tests.Include(q => q.Questions)
+            .ThenInclude(a => a.Answers)
+            .FirstOrDefaultAsync(t => t.Id == selectedAnswers[0].TestId);
+        Lesson? lesson = await _db.Lessons.FirstOrDefaultAsync(l => l.Id == test.LessonId);
+        UserLessons? userLessons = await _db.UserLessons.FirstOrDefaultAsync(ul =>
+            ul.UserId == currentUser.Id && ul.SubtopicId == lesson.SubtopicId);
+
+        int passingCount = (
+            from question in test.Questions
+            from answer in question.Answers.Where(a => a.IsCorrect)
+            join selectedAnswer in selectedAnswers on answer.Id equals selectedAnswer.AnswerId
+            select selectedAnswer
+        ).Count();
+        if (passingCount >=2)
+        {
+            userLessons.PassedLevel = lesson.LessonLevel;
+            userLessons.IsPassed = true;
+            userLessons.LessonId = lesson.Id;
+            _db.UserLessons.Update(userLessons);
+            int n = await _db.SaveChangesAsync();
+            if (n > 0)
+                return Ok("Вы прошли тест");
+        }
+        return Ok("Вы ответили неправильно");
+    }
+
+    public async Task<IActionResult> TestResultPage(bool isPassed, Test? test)
+    {
+        if (isPassed)
+        {
+            return View();
+        }
+        else
+        {
+            return View();
+        }
+    }
+
 }
---
+++ ViewModels/TestAnswersModel.cs
@@ -0,0 +1,8 @@
+namespace YouCan.Models;
+
+public class TestAnswersModel
+{
+    public int TestId { get; set; }
+    public int QuestionId { get; set; }
+    public int AnswerId { get; set; }
+}
