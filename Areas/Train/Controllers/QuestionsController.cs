289d23703a6f8bc221d66ad29516d8ddae9da832
Eldar Bakirov <e.bakirov@bakirovlab.ru>
2024-07-23 13:01:23 +0600

    Добавление представлений создания тестовых вопросов и обновление стилей

---
+++ Areas/Train/Controllers/QuestionsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YouCan.Areas.Train.Controllers;
+
+[Area("Train")]
+public class QuestionsController : Controller
+{
+    public IActionResult Create()
+    {
+        return View();
+    }
+}
---
+++ Areas/Train/Controllers/QuestionsWithTextController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YouCan.Areas.Train.Controllers;
+
+[Area("Train")]
+public class QuestionsWithTextController : Controller
+{
+    public IActionResult Create()
+    {
+        return View();
+    }
+}
