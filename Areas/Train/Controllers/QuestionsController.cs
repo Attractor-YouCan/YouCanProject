80a82120cc7362e2ece5ba33fc23b82ddc7c048e
Eldar Bakirov <e.bakirov@bakirovlab.ru>
2024-07-25 19:28:13 +0600

    Обновление действия Create контроллера Questions

--- Areas/Train/Controllers/QuestionsController.cs
+++ Areas/Train/Controllers/QuestionsController.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using YouCan.Areas.Train.Dto;
 
 namespace YouCan.Areas.Train.Controllers;
 
 [Area("Train")]
 public class QuestionsController : Controller
 {
-    public IActionResult Create()
+    [HttpGet]
+    public IActionResult Create(int subSubjectId)
     {
-        return View();
+        var model = new QuestionDto()
+        {
+            Answers = new List<AnswerDto>(),
+            SubjectId = subSubjectId
+        };
+        for(int i=0; i<5;i++)
+        {
+            model.Answers.Add(new AnswerDto());
+        }
+        return View(model);
     }
 }
