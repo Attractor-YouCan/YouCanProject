78347111408372d5429381235d49a3ef9dcb4ce2
Eldar Bakirov <e.bakirov@bakirovlab.ru>
2024-07-25 19:26:18 +0600

    Добавление Data-Transfer объектов для создания пользовательский тестовых вопросов

---
+++ Areas/Train/Dto/QuestionDto.cs
@@ -0,0 +1,10 @@
+﻿namespace YouCan.Areas.Train.Dto;
+
+public class QuestionDto
+{
+    public int SubjectId { get; set; }
+    public string? Text { get; set; }
+    public IFormFile? Image { get; set; }
+    public List<AnswerDto> Answers { get; set; }
+    public int CorrectAnswerId { get; set; }
+}
---
+++ Areas/Train/Dto/TestDto.cs
@@ -0,0 +1,13 @@
+﻿namespace YouCan.Areas.Train.Dto;
+
+public class TestDto
+{
+    public class TestQuestionDto
+    {
+        public string Content { get; set; }
+        public List<string> Answers { get; set; }
+    }
+    public int SubjectId { get; set; }
+    public string Content { get; set; }
+    public List<TestQuestionDto> Questions { get; set; }
+}
