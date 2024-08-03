9eb5b4c803dbcff6e782c782b04302f471e2805a
macbook <seyit200020@gmail.com>
2024-08-03 17:10:27 +0600

    Добавил отправку повторного кода на почту

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -257,19 +257,40 @@ public class AccountController : Controller
             ModelState.AddModelError("", "Пользователь не найден!.");
             return Json(new { success = false, errors = ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage) });
         }
-        
+
         var isCodeValid =  _twoFactorService.VerifyCode(user.Id, model.Code);
         if (!isCodeValid)
         {
             ModelState.AddModelError("", "Неправильный код!.");
             return Json(new { success = false, errors = ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage) });
         }
-        
+    
         user.EmailConfirmed = true;
         await _userManager.UpdateAsync(user);
         await _signInManager.SignInAsync(user, true);
+        
 
         return Json(new { success = true, userId = user.Id  });
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> ResendCode([FromBody] ResendCodeRequest model)
+    {
+        if (string.IsNullOrEmpty(model.Email))
+        {
+            return Json(new { success = false, errors = new[] { "Email is required." } });
+        }
+
+        var user = await _userManager.FindByEmailAsync(model.Email);
+        if (user == null)
+        {
+            return Json(new { success = false, errors = new[] { "User not found." } });
+        }
+        var (subject, message) = GenerateEmailConfirmationContentAsync(user, user.UserName);
+        EmailSender emailSender = new EmailSender();
+         emailSender.SendEmail(user.Email, subject, message);
+
+        return Json(new { success = true });
     }
     
 
