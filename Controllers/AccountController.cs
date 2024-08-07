f3466eb1bb647b8d17bf5756ad1fe48868d0a82e
macbook <seyit200020@gmail.com>
2024-08-07 16:52:11 +0600

    Сделал исправления в регистрации в Многоуровней архитектуры

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,9 +1,9 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.DotNet.Scaffolding.Shared.CodeModifier.CodeChange;
 using YouCan.Entities;
 using YouCan.Service.Service;
+using YouCan.Services.Email;
 
 namespace YouCan.Mvc;
 public class AccountController : Controller
