b191d8bfa756b526717729e87aa0d93ab05c69c6
Bakirov <82159025+bakirov-eldar@users.noreply.github.com>
2024-08-20 23:32:00 +0600

    Добавлен LessonTime.cs для хранение потраченного времени на обучение.

---
+++ YouCan.Entites/Models/LessonTime.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YouCan.Entities;
+
+namespace YouCan.Entites.Models;
+
+public class LessonTime : EntityBase
+{
+    public int UserId { get; set; }
+    public User? User { get; set; }
+    public int LessonId { get; set; }
+    public Lesson? Lesson { get; set; }
+    public TimeSpan TimeSpent { get; set; }
+}
