c6a0d1bad271759fe9091fb7b92f056ea277528b
Nariman Biiazbekov <narbiiaz@gmail.com>
2024-08-21 16:24:43 +0600

    Добавлен представление для админки

---
+++ Areas/Admin/Controllers/StatisticController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using YouCan.Entities;
+
+namespace YouCan.Areas.Admin.Controllers;
+
+public class StatisticController : Controller
+{
+    private readonly UserManager<User> _userManager;
+
+    public StatisticController(UserManager<User> userManager)
+    {
+        _userManager = userManager;
+    }
+
+
+}
