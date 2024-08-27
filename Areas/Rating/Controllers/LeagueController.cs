0e50bb4323bd21356d07a08869d0652f147a4441
macbook <seyit200020@gmail.com>
2024-08-27 15:31:37 +0600

    adding users with league

--- Areas/Rating/Controllers/LeagueController.cs
+++ Areas/Rating/Controllers/LeagueController.cs
@@ -1,8 +1,10 @@
 
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using YouCan.Entities;
 using YouCan.Repository;
+using YouCan.ViewModels;
 
 
 namespace YouCan.Areas.Rating.Controllers
@@ -11,17 +13,30 @@ namespace YouCan.Areas.Rating.Controllers
     public class LeagueController : Controller
     {
         private readonly LeagueRepository _leagueRepository;
+        private UserManager<User> _userManager;
 
-        public LeagueController(LeagueRepository leagueRepository)
+        public LeagueController(LeagueRepository leagueRepository, UserManager<User> userManager)
         {
             _leagueRepository = leagueRepository;
+            _userManager = userManager;
         }
 
         // GET: Rating/League
         public async Task<IActionResult> Index()
         {
             var leagues = _leagueRepository.GetAll().ToList();
-            return View(leagues);
+            
+            var users = await _userManager.Users
+                .Include(u => u.League) // Ensure that the League is included
+                .OrderByDescending(u => u.Rank) // Order users by their rank
+                .ToListAsync();
+            
+            var viewModel = new CombinedRatingViewModel
+            {
+                Leagues = leagues,
+                Users = users
+            };
+            return View(viewModel);
         }
 
         // GET: Rating/League/Create
