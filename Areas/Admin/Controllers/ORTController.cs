46b60014ab0400d862cb4afccb2bf5944e5db5b9
Nariman Biiazbekov <narbiiaz@gmail.com>
2024-08-30 13:42:23 +0600

    Добавлено представления для орт тестов

---
+++ Areas/Admin/Controllers/ORTController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using YouCan.Entities;
+using YouCan.Service.Service;
+
+namespace YouCan.Areas.Admin.Controllers;
+[Area("Admin")]
+public class ORTController : Controller
+{
+    private readonly ICRUDService<OrtTest> _ortManager;
+    private readonly ICRUDService<Test> _testsManager;
+    private readonly ICRUDService<OrtInstruction> _instructionsManager;
+    private readonly UserManager<User> _userManager;
+    public ORTController(UserManager<User> userManager, ICRUDService<Test> testManager,
+                         ICRUDService<OrtInstruction> instructionsManager, ICRUDService<OrtTest> ortManager)
+    {
+        _userManager = userManager;
+        _testsManager = testManager;
+        _instructionsManager = instructionsManager;
+        _ortManager = ortManager;
+    }
+    public IActionResult Index() => View(_ortManager.GetAll().ToList());
+    public async Task<IActionResult> Details(int ortId)
+    {
+        var ort = await  _ortManager.GetById(ortId);
+        if (ort != null)
+        {
+            return View(ort);   
+        }
+        return NotFound();
+    }
+}
