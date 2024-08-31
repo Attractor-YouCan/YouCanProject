a93ccf817bea5a6163fc762200ed1109a83bc480
Alexsandr <brgalex910@gmail.com>
2024-09-01 02:55:52 +0600

    Работал над отображением ударного режима. Убрал лишние стили, доработал скрипт по отображению календаря. Приблизил дизайн к макету

--- YouCan.Repository/Data/Services/AdminInitializer.cs
+++ YouCan.Repository/Data/Services/AdminInitializer.cs
@@ -78,11 +78,11 @@ public class AdminInitializer
             UserExperiences = {
             new UserExperience {Id = 1, UserId = 5, Date = new DateTime(2024, 8, 24, 0, 0, 0, DateTimeKind.Utc), ExperiencePoints = 100 },
             new UserExperience {Id = 2, UserId = 5, Date = new DateTime(2024, 8, 23, 0, 0, 0, DateTimeKind.Utc), ExperiencePoints = 75 },
-            new UserExperience {Id = 3, UserId = 5, Date = new DateTime(2024, 8, 22, 0, 0, 0, DateTimeKind.Utc), ExperiencePoints = 10 },
-            new UserExperience {Id = 4, UserId = 5, Date = new DateTime(2024, 8, 21, 0, 0, 0, DateTimeKind.Utc), ExperiencePoints = 50 },
-            new UserExperience {Id = 5, UserId = 5, Date = new DateTime(2024, 8, 20, 0, 0, 0, DateTimeKind.Utc), ExperiencePoints = 100 },
-            new UserExperience {Id = 6, UserId = 5, Date = new DateTime(2024, 8, 19, 0, 0, 0, DateTimeKind.Utc), ExperiencePoints = 35 },
-            new UserExperience {Id = 7, UserId = 5, Date = new DateTime(2024, 8, 18, 0, 0, 0, DateTimeKind.Utc), ExperiencePoints = 75 }
+            new UserExperience {Id = 3, UserId = 5, Date = new DateTime(2024, 8, 25, 0, 0, 0, DateTimeKind.Utc), ExperiencePoints = 10 },
+            new UserExperience {Id = 4, UserId = 5, Date = new DateTime(2024, 8, 26, 0, 0, 0, DateTimeKind.Utc), ExperiencePoints = 50 },
+            new UserExperience {Id = 5, UserId = 5, Date = new DateTime(2024, 8, 27, 0, 0, 0, DateTimeKind.Utc), ExperiencePoints = 100 },
+            new UserExperience {Id = 6, UserId = 5, Date = new DateTime(2024, 8, 18, 0, 0, 0, DateTimeKind.Utc), ExperiencePoints = 35 },
+            new UserExperience {Id = 7, UserId = 5, Date = new DateTime(2024, 8, 29, 0, 0, 0, DateTimeKind.Utc), ExperiencePoints = 75 }
             }
         };
         List<User> users = new List<User>() { user1, user2, user3, profileTest };
