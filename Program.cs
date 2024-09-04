f38a3b610a577d33812de3cdd92c08f510b6a4f8
Imanbekov Tologon <imanbekovtologon4@gmail.com>
2024-09-04 18:10:14 +0600

    Edited Program.cs

--- Program.cs
+++ Program.cs
@@ -19,6 +19,9 @@ builder.Services.AddControllers()
 builder.Configuration
     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
     .AddEnvironmentVariables();
+builder.Configuration
+    .AddJsonFile("appsettings.Production.json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables();
 
 string connection = builder.Configuration.GetConnectionString("DefaultConnection");
 
