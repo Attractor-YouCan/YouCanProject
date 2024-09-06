01da45f92a645bb0893f3862efdf8830d3ba7ddf
Nariman Biiazbekov <narbiiaz@gmail.com>
2024-09-06 17:48:48 +0600

    Добавлен функционал добавления нескольких вопросов к орт тесту

--- Areas/Admin/Controllers/ORTController.cs
+++ Areas/Admin/Controllers/ORTController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using YouCan.Areas.Admin.ViewModels;
 using YouCan.Entities;
 using YouCan.Service.Service;
+using YouCan.Services;
 
 namespace YouCan.Areas.Admin.Controllers;
 [Area("Admin")]
@@ -12,15 +14,22 @@ public class ORTController : Controller
     private readonly ICRUDService<OrtInstruction> _instructionsManager;
     private readonly UserManager<User> _userManager;
     private readonly ICRUDService<Subject> _subjectManager;
+    private readonly IWebHostEnvironment _env;
+    private readonly ICRUDService<Question> _questionManager;
+    private readonly ICRUDService<Answer> _answerManager;
     public ORTController(UserManager<User> userManager, ICRUDService<Test> testManager,
                          ICRUDService<OrtInstruction> instructionsManager, ICRUDService<OrtTest> ortManager,
-                         ICRUDService<Subject> subjectManager)
+                         ICRUDService<Subject> subjectManager, IWebHostEnvironment env,
+                         ICRUDService<Question> questionManager, ICRUDService<Answer> answerManager)
     {
         _userManager = userManager;
         _testsManager = testManager;
         _instructionsManager = instructionsManager;
         _ortManager = ortManager;
         _subjectManager = subjectManager;
+        _env = env;
+        _questionManager = questionManager;
+        _answerManager = answerManager;
     }
     public IActionResult Index() => View(_ortManager.GetAll().ToList());
     public async Task<IActionResult> Details(int ortId)
@@ -79,5 +88,59 @@ public class ORTController : Controller
             return RedirectToAction("Details", new { ortId = test.OrtTestId });
         }
         return BadRequest();
+    }
+
+    [HttpGet]
+    public IActionResult CreateQuestions(int testId)
+    {
+        ViewBag.TestId = testId;
+        return View();
+    }
+    [HttpPost]
+    public async Task<IActionResult> CreateQuestions([FromForm] TestsModel model)
+    {
+        W3RootFileManager fileManager = new(_env);
+        if (model != null)
+        {
+            var test = await _testsManager.GetById(model.TestId);
+            if (test != null)
+            {
+                if (model.Questions != null)
+                {
+                    foreach (var question in model.Questions)
+                    {
+                        string? questionFileName = null;
+                        if (question.Image != null)
+                        {
+                            questionFileName = await fileManager.SaveFormFileAsync("ortTestsResourses", question.Image);
+                        }
+                        Question newQuestion = new()
+                        {
+                            Instruction = question.Instruction,
+                            Content = question.Text,
+                            ImageUrl = questionFileName,
+                            IsPublished = true,
+                            Point = 1,
+                            TestId = model.TestId,
+                            SubjectId = test.SubjectId
+                        };
+                        await _questionManager.Insert(newQuestion);
+
+                        foreach (var asnwer in question.Answers)
+                        {
+                            Answer newAnswer = new()
+                            {
+                                Content = asnwer.AnswerText,
+                                IsCorrect = asnwer.IsCorrect,
+                                QuestionId = newQuestion.Id,
+                            };
+                            await _answerManager.Insert(newAnswer);
+                        }
+                    }
+                    return RedirectToAction("Details", new {ortId = test.OrtTestId});
+                }
+            }
+        }
+        return NotFound();
     }
 }
---
+++ Areas/Admin/ViewModels/TestsModel.cs
@@ -0,0 +1,9 @@
+﻿using YouCan.Entities;
+
+namespace YouCan.Areas.Admin.ViewModels;
+
+public class TestsModel
+{
+    public int TestId { get; set; } 
+    public List<QuestionModel> Questions { get; set; } 
+}
