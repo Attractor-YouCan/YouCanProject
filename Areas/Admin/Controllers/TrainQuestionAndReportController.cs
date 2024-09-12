377ed61445c168cddfd828318544ebb16c49f6b0
Askarbekov Aktan <akn392204@gmail.com>
2024-09-12 23:45:09 +0600

    Добавил возможность редактирования вопроса с ответами

--- Areas/Admin/Controllers/TrainQuestionAndReportController.cs
+++ Areas/Admin/Controllers/TrainQuestionAndReportController.cs
@@ -90,4 +90,58 @@ public class TrainQuestionAndReportController : Controller
         var reports = _questionReportService.GetAll().ToList();
         return View(reports);
     }
+    
+    [HttpGet]
+    public async Task<IActionResult> Edit(int id)
+    {
+        var question = await _questionService.GetById(id);
+        if (question == null)
+        {
+            return NotFound();
+        }
+
+        var subjects = _subjectService.GetAll().Where(s => s.SubjectType == SubjectType.Child).ToList();
+        var selectedSubject = subjects.FirstOrDefault(s => s.Id == question.SubjectId);
+        if (selectedSubject != null)
+        {
+            subjects.Remove(selectedSubject);
+            subjects.Insert(0, selectedSubject);  
+        }
+
+        ViewBag.Subjects = subjects; 
+        return View(question);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(Question model)
+    {
+        if (!ModelState.IsValid)
+        {
+            ViewBag.Subjects = _subjectService.GetAll();
+            return View(model);
+        }
+
+        // Обновляем данные вопроса
+        var question = await _questionService.GetById(Convert.ToInt32(model.Id));
+        if (question == null)
+        {
+            return NotFound();
+        }
+
+        question.Content = model.Content;
+        question.Instruction = model.Instruction;
+        question.SubjectId = model.SubjectId;
+        question.ImageUrl = model.ImageUrl;
+
+        // Обновляем ответы
+        for (int i = 0; i < model.Answers.Count; i++)
+        {
+            question.Answers[i].Content = model.Answers[i].Content;
+            question.Answers[i].IsCorrect = model.Answers[i].IsCorrect;
+        }
+
+        await _questionService.Update(question);
+        return RedirectToAction("Details", new { id = question.Id });
+    }
+
 }
