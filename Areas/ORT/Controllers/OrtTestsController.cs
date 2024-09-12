3f18ccd04e7de39b8ddfa460ecf330d85f4ec319
Muratov Kylymbek <kylymbek.muratov.00@gmail.com>
2024-09-12 20:03:34 +0600

    Добавил функционал отображения всех пробных тестов для админа  и юзерам после подписки, а остальным отображается только первый пробный тест. Убрал лишние коды.

--- Areas/ORT/Controllers/OrtTestsController.cs
+++ Areas/ORT/Controllers/OrtTestsController.cs
@@ -43,6 +43,10 @@ public class OrtTestsController : Controller
             else
                 ViewBag.CurrentOrtTestLevel = userOrtTest.OrtTest.OrtLevel - 1;
         }
+
+        var userRoles = await _userManager.GetRolesAsync(currUser);
+        ViewBag.UserRoles = userRoles;
+        
         List<OrtTest> ortTests = _ortTestService.GetAll().ToList();
         return View(ortTests);
     }
@@ -53,22 +57,6 @@ public class OrtTestsController : Controller
         OrtTest? ortTest = await _ortTestService.GetById(ortTestId);
         if (ortTest == null)
             return NotFound("no ort test");
-        User? curUser = await _userManager.GetUserAsync(User);
-        if (curUser == null)
-            RedirectToAction("Login", "Account");
-        UserOrtTest? userOrtTest = await _userOrtTestService.GetById(curUser.Id);
-        int currentLevel = 0;
-        if (userOrtTest.OrtTest != null)
-        {
-            if (userOrtTest.IsPassed)
-                currentLevel = (int)userOrtTest.OrtTest.OrtLevel;
-            else
-                currentLevel = (int)(userOrtTest.OrtTest.OrtLevel - 1);
-            if (currentLevel + 1 >= ortTest.OrtLevel)
-                return View(ortTest);
-        }
-        else if(userOrtTest.PassedLevel + 1 >= ortTest.OrtLevel)
-            return View(ortTest);
-        return RedirectToAction("Index");
+        return View(ortTest);
     }
 }
