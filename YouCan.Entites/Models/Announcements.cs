5497860307dffc3a8e495e81e544c4375f53a4cf
Nariman Biiazbekov <narbiiaz@gmail.com>
2024-09-13 16:01:39 +0600

    Создана модель обьявлений Announcements

---
+++ YouCan.Entites/Models/Announcements.cs
@@ -0,0 +1,10 @@
+﻿using YouCan.Entities;
+
+namespace YouCan.Entites.Models;
+
+public class Announcements : EntityBase
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime Created { get; set; } = DateTime.UtcNow;
+}
