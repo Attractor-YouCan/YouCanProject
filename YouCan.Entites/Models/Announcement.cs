39494077ea2c8daedd769cf574455a154fa667d2
Nariman Biiazbekov <narbiiaz@gmail.com>
2024-09-13 16:43:16 +0600

    Добавлено представление для объявлений

--- YouCan.Entites/Models/Announcement.cs
+++ YouCan.Entites/Models/Announcement.cs
@@ -6,5 +6,6 @@ public class Announcement : EntityBase
 {
     public string Title { get; set; }
     public string Description { get; set; }
+    public string? ImageUrl { get; set; }
     public DateTime Created { get; set; } = DateTime.UtcNow;
 }
