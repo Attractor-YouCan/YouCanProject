7aa839baae7fff6aca412036008f81ff3bc07167
Bakirov <82159025+bakirov-eldar@users.noreply.github.com>
2024-09-18 18:08:26 +0600

    Скорректированы названия предметов.

--- YouCan.Repository/Data/Services/SubjectInitializer.cs
+++ YouCan.Repository/Data/Services/SubjectInitializer.cs
@@ -26,21 +26,22 @@ public class SubjectInitializer
             new Subject(){Id = 9, Name = "Дополнение предложений", ImageUrl = "/topicImages/readUnderstIcon.png", ParentId = 5, SubjectType = SubjectType.Child}
         );
 
-        // ������������� ������ ��� SubjectLocalization � ��������� �������� ����� SubjectId
         _modelBuilder.Entity<SubjectLocalization>().HasData(
-            new SubjectLocalization() { Id = 1, SubjectId = 1, Culture = "ru", Title = "����������" },
-            new SubjectLocalization() { Id = 2, SubjectId = 1, Culture = "ky", Title = "����������" },
-            new SubjectLocalization() { Id = 3, SubjectId = 2, Culture = "ru", Title = "������� ����" },
-            new SubjectLocalization() { Id = 4, SubjectId = 2, Culture = "ky", Title = "���� ����" },
-            new SubjectLocalization() { Id = 5, SubjectId = 3, Culture = "ru", Title = "���������� 1" },
-            new SubjectLocalization() { Id = 6, SubjectId = 3, Culture = "ky", Title = "���������� 1" },
-            new SubjectLocalization() { Id = 7, SubjectId = 4, Culture = "ru", Title = "������� ����" },
-            new SubjectLocalization() { Id = 8, SubjectId = 4, Culture = "ky", Title = "���� ����" },
-            new SubjectLocalization() { Id = 9, SubjectId = 5, Culture = "ru", Title = "��������" },
-            new SubjectLocalization() { Id = 10, SubjectId = 6, Culture = "ru", Title = "����������" },
-            new SubjectLocalization() { Id = 11, SubjectId = 7, Culture = "ru", Title = "������ � ���������" },
-            new SubjectLocalization() { Id = 12, SubjectId = 8, Culture = "ru", Title = "��������" },
-            new SubjectLocalization() { Id = 13, SubjectId = 9, Culture = "ru", Title = "���������� �����������" }
+            new SubjectLocalization() { Id = 1, SubjectId = 1, Culture = "ru", Title = "Математика" },
+            new SubjectLocalization() { Id = 2, SubjectId = 1, Culture = "ky", Title = "Математика" },
+            new SubjectLocalization() { Id = 3, SubjectId = 2, Culture = "ru", Title = "Русский язык" },
+            new SubjectLocalization() { Id = 4, SubjectId = 2, Culture = "ky", Title = "Орус тили" },
+            new SubjectLocalization() { Id = 5, SubjectId = 3, Culture = "ru", Title = "Математика 1" },
+            new SubjectLocalization() { Id = 6, SubjectId = 3, Culture = "ky", Title = "Математика 1" },
+            new SubjectLocalization() { Id = 7, SubjectId = 4, Culture = "ru", Title = "Математика 2" },
+            new SubjectLocalization() { Id = 8, SubjectId = 4, Culture = "ky", Title = "Математика 2" },
+            new SubjectLocalization() { Id = 9, SubjectId = 5, Culture = "ru", Title = "Аналогия" },
+            new SubjectLocalization() { Id = 10, SubjectId = 6, Culture = "ru", Title = "Грамматика" },
+            new SubjectLocalization() { Id = 11, SubjectId = 7, Culture = "ru", Title = "Чтение и понимание" },
+            new SubjectLocalization() { Id = 12, SubjectId = 7, Culture = "ky", Title = "Окуу жана тушунуу" },
+            new SubjectLocalization() { Id = 13, SubjectId = 8, Culture = "ru", Title = "Аналогия" },
+            new SubjectLocalization() { Id = 14, SubjectId = 9, Culture = "ru", Title = "Дополнение предложений" },
+            new SubjectLocalization() { Id = 15, SubjectId = 9, Culture = "ky", Title = "Суйломго толуктоо" }
         );
 
     }
